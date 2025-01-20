0f87d16f50dd16c4e62703ecf32b37eac54c592d
ivan <yankoval@gmail.com>
2025-01-20 13:54:04 +0300

    moex.py - Change interface SecInfo tuple returned levelsGenerator.py - updated to new moex.py api/ Index and stock tikers now avialoble.

--- moex.py
+++ moex.py
@@ -36,6 +36,12 @@ baseUrl = 'https://iss.moex.com/iss'
 moexTypes = dict()
 marketSheme = dict()
 
+def getTyped(d:dict,s:str=''):
+    """ return namedtuple structure from d in iss format with s key present in it"""
+    if s:
+        return namedtuple(s, d[s]['columns'])(*d[s]['data'][0])
+    return namedtuple('sec', [k for k in d.keys() if d.get(k, {}).get('data')])(
+        *[getTyped(d, s=k) for k in d.keys() if d.get(k, {}).get('data')])
 def moexMarketSheme():
     """ read base market data like boards engines etc"""
     # index.xml
@@ -84,13 +90,16 @@ def loadCandlesPage(sec:str= 'SBER', dateFrom:date=None, dateTo:date =None, inte
     # load data in iss.moex.com format, sec = 'SBER', interval in ['1','10','60','24','7','31']
     dateFrom = date.today() - timedelta(days=1) if dateFrom is None else dateFrom
     dateTo = dateFrom + timedelta(days=1) if dateTo is None else dateTo
-    engines, markets, boards = 'futures', 'forts', 'rfud'
-    assert  interval in ['1','10','60','24','7','31'] # Check interval
+    if type(sec) is str:
+        sec, engines, markets, boards, secInfoTraded  = secInfo(sec)
+        if not isinstance(sec,tuple):
+            raise Exception('Error loading candles page')
+    assert interval in ['1','10','60','24','7','31'] # Check interval
     # print(dateFrom,dateTo)
     # apiKey = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJaVHA2Tjg1ekE4YTBFVDZ5SFBTajJ2V0ZldzNOc2xiSVR2bnVaYWlSNS1NIn0.eyJleHAiOjE3MzQ4NzY0MTMsImlhdCI6MTczMjI4NDQxMywiYXV0aF90aW1lIjoxNzMyMjg0MDE0LCJqdGkiOiIyNmUwZjczNS02ODA1LTQ2OWQtOGQzZi00N2VkMGRhZmZkNjIiLCJpc3MiOiJodHRwczovL3NzbzIubW9leC5jb20vYXV0aC9yZWFsbXMvY3JhbWwiLCJhdWQiOlsiYWNjb3VudCIsImlzcyJdLCJzdWIiOiJmOjBiYTZhOGYwLWMzOGEtNDlkNi1iYTBlLTg1NmYxZmU0YmY3ZTozNmE1ZTczZS05OTViLTRiNTUtOWVlMS0zMmE5NGM3NTljZDUiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJpc3MiLCJzZXNzaW9uX3N0YXRlIjoiYjBjOTdjYWUtNjVhMi00ODUyLTg4OTEtZTVmNDg2ZDAyNGU4IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBvZmZsaW5lX2FjY2VzcyBpc3NfYWxnb3BhY2sgcHJvZmlsZSBlbWFpbCIsInNpZCI6ImIwYzk3Y2FlLTY1YTItNDg1Mi04ODkxLWU1ZjQ4NmQwMjRlOCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzX3Blcm1pc3Npb25zIjoiMTM3LCAxMzgsIDEzOSwgMTQwLCAxNjUsIDE2NiwgMTY3LCAxNjgsIDMyOSwgNDIxIiwibmFtZSI6ItCY0LLQsNC9INCa0LjRgdC10LvRkdCyIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiMzZhNWU3M2UtOTk1Yi00YjU1LTllZTEtMzJhOTRjNzU5Y2Q1IiwiZ2l2ZW5fbmFtZSI6ItCY0LLQsNC9IiwiZmFtaWx5X25hbWUiOiLQmtC40YHQtdC70ZHQsiJ9.iwQXQrn6tU1R5Ek1HS_ccNiKNuTPs8toPkjjvxDN2OTvIU2kzUamsRYfrXMwmyRmFSvpByWc0E_1-uhzRuak0RsXJ9xKogCQGKiNEbBf7ELDA45FcQM56hhWOrMIYNDC57Gbi-stTELzMGmBijg5tt2vMn4q9VKaLoYMJJ3yuzgTnWz1VqpKCSAGYokT1k-17eQYSFpWPdR61G09-Nezy7P01XPGDuket0-9o5eUar15x07bwWrulB8VXZrAcQ7Z75XM5qJyUSXgtdenCHgmXV3b5WzfAoNrL5AXvutzk5YtyJEFpyi3oOUIjTXTuZGtPfB-DrXmtlpo7Ydm1O3QQw'
 
     # url = "https://apim.moex.com/iss/datashop/algopack/eq/obstats.json?date=2024-10-15"
-    url = (f'{baseUrl}/engines/futures/markets/forts/boards/rfud/securities/{sec}/candles.json?from={dateFrom}'
+    url = (f'{baseUrl}/engines/{engines}/markets/{markets}/boards/{boards}/securities/{sec.secid}/candles.json?from={dateFrom}'
            f'&till={dateTo}&interval={interval}&start={start}')
 
     headers = {
@@ -142,10 +151,10 @@ candles = toDfCandles(loadCandlesPage) # multiformat variant (df|list)
 
 
 def secInfo(secid):
-    """ securities information and data from iss """
+    """ securities information and data from iss return tuple secInf, engine, market, board, secInfoTraded  """
     # load data in iss.moex.com format
     # baseUrl = 'https://iss.moex.com/iss'
-    engines, markets, boards, interval = 'futures', 'forts', 'rfud', 60
+    # engines, markets, boards, interval = 'futures', 'forts', 'rfud', 60
     # print(dateFrom,dateTo)
     # url = "https://iss.moex.com/iss/engines/futures/markets/forts/boards/rfud/securities/MXZ4.json"
     #https://iss.moex.com/iss/securities.json?q=MXZ4
@@ -165,14 +174,15 @@ def secInfo(secid):
               return None
           row = [row for row in res['securities']['data'] if secid in [row[1],row[2],row[5]] ]
           if any(row):
-            s = namedtuple('sec', res['securities']['columns'])
-            secInf = s(*row[0])
+            # typeSecuritiesFound = namedtuple('sec', res['securities']['columns'])
+            res['securities']['data'] = row
+            secInf = getTyped(res,'securities')
             boardGroupInfo = [row for row in marketSheme['boardgroups'] if row.name == secInf.group]
             if len(boardGroupInfo) != 1:
                 return None
             boardGroupInfo = boardGroupInfo[0]
             engine, market, board = boardGroupInfo.trade_engine_name, boardGroupInfo.market_name, secInf.primary_boardid
-            return secInf, engine, market, board
+            # return secInf, engine, market, board
           else:
               return None
         except json.decoder.JSONDecodeError:
@@ -180,23 +190,26 @@ def secInfo(secid):
     else:
         print(f'Error request responce : {response.status_code}')
         raise Exception
-    # url = f'{baseUrl}/history/engines/{engines}/markets/{markets}/boards/{boards}/securities/{secid}.json'
-    # response = requests.request("GET", url, headers=headers)
-    # if response.status_code == 200:
-    #     try:
-    #         # print(response.json)
-    #         res = json.loads(response.text)
-    #         if not res['history']['data']:
-    #             print(f'Error loading {secid}')
-    #             return None
-    #     except json.decoder.JSONDecodeError:
-    #         print(f'Error parsr json responce: {response.text[:50]}')
-    # else:
-    #     print(f'Error request responce : {response.status_code}')
-    #     raise Exception
-    # s = namedtuple('sec', res['history']['columns'])
-    # secInf = s(*res['history']['data'][0])
-    # return secInf
+    if secInf.is_traded == 1:
+        url = f'{baseUrl}/engines/{engine}/markets/{market}/boards/{board}/securities/{secid}.json'
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code == 200:
+            try:
+                # print(response.json)
+                res = response.json()
+                if not res.get('securities',{}).get('data',{}):
+                    print(f'Error loading {secid}')
+                    raise 'Error loading secInfoTraded'
+                secInfTraded = getTyped(res)
+            except json.decoder.JSONDecodeError:
+                print(f'Error parsr json responce: {response.text[:50]}')
+                raise Exception
+        else:
+            print(f'Error request responce : {response.status_code}')
+            raise Exception
+    else:
+        secInfTraded = None
+    return secInf, engine, market, board, secInfTraded
 def futSeries():
     """ futures series information from iss """
   # https://iss.moex.com/iss/statistics/engines/futures/markets/forts/series.json
