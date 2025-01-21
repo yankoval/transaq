745f30d6f225271d8d29695629a2f49de03fb320
ivan <yankoval@gmail.com>
2025-01-21 09:23:07 +0300

    moex.py - Change interface SecInfo tuple returned levelsGenerator.py - updated to new moex.py api/ Index and stock tikers now avialoble.

--- levelsGenerator.py
+++ levelsGenerator.py
@@ -204,7 +204,7 @@ def main(tikers=[], output_filepath='.//', days=1, timeFrame='1', logger=logging
         dfS = indicators(dfS)
         levels = plotDay(dfS,df,plot=False, validateLearnRatio=1) # get leve
         try:# ls
-            if tikInfo.type  in ['futures_forts']:
+            if tikInfo.type  in ['futures_forts','futures']:
                 levels = list(map(lambda x: int((x//tikInfoTraded.securities.MINSTEP)*tikInfoTraded.securities.MINSTEP), levels)) # round as ticker price step
             elif tikInfo.type in ['stock_index','stock_shares','common_share']:
                 levels = list(map(lambda x: round(x, tikInfoTraded.securities.DECIMALS), levels))
