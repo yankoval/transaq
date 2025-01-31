9683269d578caf902bd05784cc48f2cace459c57
ivan <yankoval@gmail.com>
2025-01-31 22:49:19 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='market',
     version='1.0',
-    packages=[''],
+    packages=find_packages(),#[''],
     url='',
     license='',
     author='ivan',
